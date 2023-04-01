d354c7ba4491db4d8e545877376920ccf33423dc
Vita Fomin <vita.bukhalo@gmail.com>
2023-04-01 14:55:23 -0400

    added gain var to JS

---
+++ assests/script.js
@@ -0,0 +1,36 @@
+var gainHead = $(".gain-head")
+var gainInfo = $(".gain-info")
+var gainInput = $(".gain-input")
+var gainWeight = $(".gain-weight")
+var gainHeight = $(".gain-height")
+var gainAge = $(".gain-age")
+var gainGender = $(".gain-gender")
+var gainSubmit = $(".btn-gain")
+var gainBmi = $(".gain-bmi")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
