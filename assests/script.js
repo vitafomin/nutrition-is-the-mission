4ace64d8a3b5fb9ab2c989c9f9208c18c407dbc2
Vita Fomin <vita.bukhalo@gmail.com>
2023-04-03 20:24:03 -0400

    added fectch method to my function and used the data from it

--- assests/script.js
+++ assests/script.js
@@ -1,18 +1,54 @@
 var gainHead = $(".gain-head")
 var gainInfo = $(".gain-info")
 var gainInput = $(".gain-input")
-var gainWeight = $(".gain-weight")
-var gainHeight = $(".gain-height")
-var gainAge = $(".gain-age")
 var gainGender = $(".gain-gender")
 var gainSubmit = $(".btn-gain")
-var gainBmi = $(".gain-bmi")
-var gainWeightValue = $(".wei-value")
-var gainHeightValue = $(".hei-value")
-var gainAgeValue = $(".age-value")
-var gainGenderValue = $(".gen-value")
+var gainBmiEl = $(".gain-bmi")
+var gainWeightEl = $(".wei-value")
+var gainHeightEl = $(".hei-value")
+var gainAgeEl = $(".age-value")
+var gainGenderEl = $(".gen-value")
+
+function gainBmi(event, age, weight, height) {
+    event.preventDefault();
+
+    var options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '3eb722f518mshee8759df28c4085p1c9e92jsnfda40a8170a9',
+            'X-RapidAPI-Host': 'fitness-calculator.p.rapidapi.com'
+        }
+    };
+
+    var bmiUrl = ("https://fitness-calculator.p.rapidapi.com/bmi?age=" + age + "&weight=" + weight + "&height=" + height);
+
+    console.log(bmiUrl);
+    fetch(bmiUrl, options)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        var bmiData = data.data;
+        console.log(bmiData)
+
+        gainBmiEl.text("Your BMI is " + bmiData.bmi + ", your health class is considered " + bmiData.health);
+
+    });
 
 
+
+    
+    
+}
+
+gainSubmit.on("click", function(event) {
+    event.preventDefault();
+    gainBmi(event, gainAgeEl.val().trim(), gainWeightEl.val().trim(), gainHeightEl.val().trim())
+    console.log(gainAgeEl.val())
+    console.log(gainWeightEl.val())
+    console.log(gainHeightEl.val())
+})
 
 
 
